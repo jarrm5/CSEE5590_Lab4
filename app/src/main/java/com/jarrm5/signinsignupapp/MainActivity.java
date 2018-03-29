42107b6ffe2bef7b66fcf686ac2423fa2e209b6a
jarrm5 <rinellaj2448@gmail.com>
2018-03-29 12:19:10 -0500

    test firebase auth code

--- app/src/main/java/com/jarrm5/signinsignupapp/MainActivity.java
+++ app/src/main/java/com/jarrm5/signinsignupapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.jarrm5.signinsignupapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -13,7 +16,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,15 +29,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     GoogleSignInClient googleSignInClient;
     SignInButton googleSignIn;
 
+    private FirebaseAuth mAuth;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
     private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEmailField = findViewById(R.id.field_email);
+        mPasswordField = findViewById(R.id.field_password);
+
         googleSignIn = findViewById(R.id.google_sign_in_button);
         googleSignIn.setOnClickListener(this);
+
+        findViewById(R.id.email_sign_in_button).setOnClickListener(this);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -39,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Build a GoogleSignInClient with the options specified by googleSignInOptions.
         googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -47,9 +68,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.google_sign_in_button:
                 doGoogleSignIn();
                 break;
+            case R.id.email_sign_in_button:
+                signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+                break;
             default:
                 break;
         }
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        /*if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();*/
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            String email = user.getEmail();
+                            String name = user.getDisplayName();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        /*if (!task.isSuccessful()) {
+                            mStatusTextView.setText(R.string.auth_failed);
+                        }
+                        hideProgressDialog();*/
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
     }
 
 
@@ -81,9 +144,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("Hi", "signInResult:failed code=" + e.getStatusCode());
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
             //updateUI(null);
         }
     }
+
+
 
 }
